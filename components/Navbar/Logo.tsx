4682ca15c0e528b50481150c25ff517d0662c305
jgnacio <jgnaciogomez406@gmail.com>
2023-04-21 13:01:51 -0700

    Add logo component

---
+++ components/Navbar/Logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className="flex flex-col justify-center">
+      <span className={"text-xl font-black"}>
+        {"</jgnacio>"}
+      </span>
+    </div>
+  );
+}
