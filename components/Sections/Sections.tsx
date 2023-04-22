1c7900f604eb21904ce9b9742eebedbca5dfba7e
jgnacio <jgnaciogomez@gmail.com>
2023-04-22 15:08:13 -0300

    add: Sections.tsx

---
+++ components/Sections/Sections.tsx
@@ -0,0 +1,10 @@
+import About from "./About/About";
+export default function Sections() {
+  return (
+    <div className="flex w-full justify-center bg-black ">
+      <div className="container">
+        <About></About>
+      </div>
+    </div>
+  );
+}
