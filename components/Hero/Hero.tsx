b14c6c97748918ed44b5b5c9b3da7fd835cbc244
jgnacio <jgnaciogomez@gmail.com>
2023-04-22 15:07:24 -0300

    fix: overflow background. add: display component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,23 +1,28 @@
 import Link from "next/link";
 import { Tinos } from "next/font/google";
+import Display from "../P5/Display";
 
 const tinosFontBold = Tinos({
   weight: ["700"],
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 
 export default function Hero() {
   return (
-    <div>
-      <div className="h-screen w-screen z-20 absolute bg-shadow-black blur-3xl" />
-      <div className="flex h-screen w-screen z-40 absolute justify-center">
-        <div className="flex flex-col justify-center font-black">
-          <p className={"text-6xl sm:text-8xl"}>Welcome to</p>
-          <Link className=" text-center underline" href="#about">
-            jgnacio portfolio
-          </Link>
+    <>
+      <div className="flex w-full bg-black justify-center">
+        <div className="container flex justify-center">
+          <div className="flex flex-col w-full h-full absolute justify-center items-center  bg-shadow-black">
+            <p className="text-6xl sm:text-7xl tracking-tighter text-center ">
+              Welcome to the
+            </p>
+            <Link className=" text-center underline" href="#about">
+              jgnacio portfolio
+            </Link>
+          </div>
+          <Display />
         </div>
       </div>
-    </div>
+    </>
   );
 }
